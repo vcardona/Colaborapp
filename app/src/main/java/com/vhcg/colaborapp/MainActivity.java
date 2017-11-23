ce654d1d896537d2c7824e2efedc3ceed3faa00b
vcardona <victorhugocardonagomez@gmail.com>
2017-11-23 10:08:25 -0500

    Funcionamiento basico montado
    En este punto la aplicación tiene la interfaz básica que se pidió,
    simplemente una VideoView, dos botones, uno para grabar el video y otro
    para reproducirlo, se esta esperando que se realicen las pruebas, para
    continuar implementando lo que esta en diseño.

--- app/src/main/java/com/vhcg/colaborapp/MainActivity.java
+++ app/src/main/java/com/vhcg/colaborapp/MainActivity.java
@@ -1,13 +1,64 @@
 package com.vhcg.colaborapp;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+
+   private Button mRecordView, mPlayView;
+   private VideoView mVideoView;
+   private int ACTIVITY_START_CAMERA_APP = 0;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mRecordView = findViewById(R.id.recordButton);
+        mPlayView = findViewById(R.id.playButton);
+        mVideoView = findViewById(R.id.videoView);
+
+        mRecordView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent callVideoAppIntent = new Intent();
+                callVideoAppIntent.setAction(MediaStore.ACTION_VIDEO_CAPTURE);
+
+                startActivityForResult(callVideoAppIntent, ACTIVITY_START_CAMERA_APP);
+            }
+        });
+
+        mPlayView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                mVideoView.start();
+
+            }
+        });
+
     }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if(requestCode == ACTIVITY_START_CAMERA_APP && resultCode == RESULT_OK)
+        {
+            Uri videoUri = data.getData();
+            mVideoView.setVideoURI(videoUri);
+        }
+    }
+
+
 }
